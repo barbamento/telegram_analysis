f45acdf9926d5e3e70f3defaadf4ee5d40da04bf
barbamento <lorenzo.alvisi@studio.unibo.it>
2022-11-09 12:43:21 +0100

    python switched to 3.9

--- db_interface/db_interface.py
+++ db_interface/db_interface.py
@@ -1,4 +1,6 @@
 import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
 
 class database_postgres:
     def __init__(self,name:str,host:str,user:str,password:str,port:int=5432):
@@ -7,6 +9,9 @@ class database_postgres:
         self.user=user
         self.password=password
         self.port=port
+
+    def get_engine(self):
+        engine = create_engine('postgresql://postgres:postgres@host:port/dbname?gssencmode=disable')
 
     def get_connection(self):
         return psycopg2.connect(
@@ -20,11 +25,18 @@ class database_postgres:
     def execute_statement(self,statement:str):
         conn=self.get_connection()
         cur=conn.cursor()
-        print(cur.execute(statement))
+        cur.execute(statement)
+        conn.commit()
 
-    def create_table(self,table_name:str):
-        pass
+    def create_table(self,table_name:str,columns:dict[str,str]):
+        create_statement=f"CREATE TABLE {table_name} "
+        columns_statement="( "
+        for col in columns.keys():
+            columns_statement+=f"{col} {columns[col]},"
+        columns_statement=columns_statement.removesuffix(",")+");"
+        self.execute_statement(create_statement+columns_statement)
 
 if __name__=="__main__":
-    db=database_postgres("telegram_scraper","localhost","admin","admin")
-    db.execute_statement("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+    db=database_postgres("telegram_scraper","127.0.0.1","admin","admin")
+    #db.execute_statement("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+    db.create_table(table_name="channels_dumped",columns={"id":"integer","name":"varchar"})
