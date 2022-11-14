43372211ced341b48f04d1e04b33239c08e9fda9
barbamento <lorenzo.alvisi@studio.unibo.it>
2022-11-14 09:48:10 +0100

    db_interface created and redo saving method

--- telegram_db/db_interface.py
+++ telegram_db/db_interface.py
@@ -1,6 +1,40 @@
 import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine
+columns={
+    "_":"varchar",
+    "id":"integer",
+    "peer_id":"varchar",
+    "date":"timestamp",
+    "message":"varchar",
+    "out":"bool",
+    "mentioned":"bool",
+    "media_unread":"bool",
+    "silent":"bool",
+    "post":"bool",
+    "from_scheduled":"varchar",
+    "legacy":"bool",
+    "edit_hide":"varchar",
+    "pinned":"varchar",
+    "noforwards":"varchar",
+    "from_id":"varchar",
+    "fwd_from":"varchar",
+    "via_bot_id":"varchar",
+    "reply_to":"varchar",
+    "media":"varchar",
+    "reply_markup":"varchar",
+    "entities":"varchar",
+    "views":"float",
+    "forwards":"float",
+    "replies":"float",
+    "edit_date":"timestamp",
+    "post_author":"varchar",
+    "grouped_id":"float",
+    "reactions":"varchar",
+    "restriction_reason":"varchar",
+    "ttl_period":"varchar",
+    "action":"varchar",
+}
 
 class database_postgres:
     def __init__(self,name:str,host:str,user:str,password:str,port:int=5432):
@@ -11,7 +45,14 @@ class database_postgres:
         self.port=port
 
     def get_engine(self):
-        engine = create_engine('postgresql://postgres:postgres@host:port/dbname?gssencmode=disable')
+        statement=(
+            f"postgresql+psycopg2://{self.user}:{self.password}@"
+            f"{self.host}:{self.port}/{self.name}"
+            "?gssencmode=disable"
+        )
+        db=create_engine(statement)
+        conn=db.connect()
+        return conn
 
     def get_connection(self):
         return psycopg2.connect(
@@ -36,7 +77,10 @@ class database_postgres:
         columns_statement=columns_statement.removesuffix(",")+");"
         self.execute_statement(create_statement+columns_statement)
 
+    def create_telegram_table(self,name:str):
+        self.create_table(name,columns=columns)
+
 if __name__=="__main__":
     db=database_postgres("telegram_scraper","127.0.0.1","admin","admin")
     #db.execute_statement("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-    db.create_table(table_name="channels_dumped",columns={"id":"integer","name":"varchar"})
+    db.create_table(table_name="dumped_channels",columns={"id":"integer","name":"varchar","datetime":"timestamp"})
--- telegram_db/telegram.py
+++ telegram_db/telegram.py
@@ -6,10 +6,14 @@ from telethon import TelegramClient as T
 from key.key import API
 import pandas as pd
 import numpy as np
+import datetime
+import json
 import os
 from typing import Union,Iterable,Callable
 import shutil
 import re
+from db_interface import database_postgres as db
+from db_interface import columns as db_columns
 
 class Telegram_Scraper:
     paths={
@@ -17,18 +21,21 @@ class Telegram_Scraper:
         "dataset_path":"dataset",
         "error_path":"errors",
     }
+    empty_table=pd.DataFrame(columns=[""])
     def __init__(self,start_from:Union[str,int],**args):
+        self.db=db("telegram_scraper","127.0.0.1","admin","admin")
+        self.pd_conn=self.db.get_engine()
         self.inizialization(**args)
         with TelegramClient("tmp", API().app_id, API().api_hash) as client:
             channel_entities=[client.get_entity(start_from)]
         channel_name=channel_entities[0].title
         d=0
         while 1:
-
             forwarded_channels=[]
             for public_channel in channel_entities:
                 print(f"Working on {public_channel.title}, at distace {d} from {channel_name}")
                 dump=self.dump_channels(public_channel)
+                exit()
                 print(f"Channel {public_channel.title} dumped")
                 if "fwd_from" in dump.columns:
                     forwarded_channels+=list(np.unique([
@@ -36,7 +43,8 @@ class Telegram_Scraper:
                         for i in dump[dump["fwd_from"].notna()]["fwd_from"] 
                         if "channel_id" in i
                     ]))
-            forwarded_channels=self.check_forwarded(forwarded_channels)
+                forwarded_channels+=self.get_forwarded_channels(public_channel)
+            forwarded_channels=set(forwarded_channels)
             print(forwarded_channels)
             exit()
             d+=1
@@ -117,62 +125,80 @@ class Telegram_Scraper:
         if os.listdir("errors")!=[]:
             for i in os.listdir("errors"):
                 shutil.rmtree(f"errors/{i}",ignore_errors=True)
+        try:
+            pd.read_sql("select * from dumped_channels where 1=2",con=self.pd_conn)
+        except Exception as e:
+            print(e)
+            self.db.create_table(table_name="dumped_channels",columns={"id":"integer","name":"varchar","datetime":"timestamp"})
+
         print("directory loaded & cleaned")
 
-    def dump_channels(self,channel_entity,limit:int=100,safe_mode:bool=True):
-        channel_username=re.sub('/', ' ', channel_entity.title)
-        if channel_username=="bestimeline":
-            return pd.read_csv("db/bestimeline.csv")
-        existing_channels=[i.removesuffix(".csv") for i in os.listdir(self.paths["database_path"])]
-        if channel_username in existing_channels:
-            print("Channel already exists. Updating existing files")
-            posts=pd.read_csv(f"{self.paths['database_path']}/{channel_username}.csv")
-            if safe_mode:
-                return posts
-            offset=max(posts["id"])+limit
+    def dump_channels(self,channel_entity,limit:int=100):
+        channel_username=channel_entity.title
+        channel_id=channel_entity.id
+        existing_channels=self.get_existing_channels(conn=self.pd_conn)
+        if (
+            (channel_username in existing_channels["name"].to_list()) 
+            or (channel_id in existing_channels["id"].to_list())
+        ):
+            offset=pd.read_sql(
+                f"SELECT id FROM id_{channel_id}",con=self.pd_conn
+            )["id"].max()+1+limit#check
+            print(offset)
         else:
-            print(f"{channel_username} is a new channel. Dumping all messages")
-            posts=pd.DataFrame()
+            pd.DataFrame(
+                [[channel_id,channel_username,datetime.datetime.now()]],
+                columns=["id","name","datetime"],
+            ).to_sql("dumped_channels",con=self.pd_conn,index=False,if_exists="append")
+            self.db.create_telegram_table(f"id_{channel_id}")
+
             offset=1+limit
+        keep_on_going=True
+        while keep_on_going:
+            with TelegramClient("tmp", API().app_id, API().api_hash) as client:
+                keep_on_going=True
+                i=0
+                while keep_on_going:
+                    tmp:ChannelMessages = client(GetHistoryRequest(
+                        peer=channel_entity,
+                        limit=100,
+                        offset_date=None,
+                        offset_id=offset,
+                        max_id=0,
+                        min_id=offset-limit-1,
+                        add_offset=0,
+                        hash=0))
+                    results=pd.DataFrame(columns=db_columns.keys()).from_records(tmp.to_dict()["messages"])
+                    results=results.sort_values(by="id",ascending=True)
+                    results=results.astype(dict([(i,"string") for i in db_columns.keys() if ((db_columns[i] in ["varchar","text"]) and (i in results.columns)) ]))
+                    keep_on_going=results["id"].min()>=offset-limit
+                    print(f"min : {results['id'].min()}")
+                    print(f"offset : {offset-limit}")
+                    print(results)
+
+                    results=results[results["id"]>=offset-limit]
+                    offset=results["id"].max()+1+limit
+                    results.to_sql(f"id_{channel_id}",con=self.pd_conn,index=False,if_exists="append")
+                    i=i+1
+                    print(keep_on_going)
+                    if i==2:
+                        exit()
+        return channel_entity
+
+    def get_forwarded_channels(self,channel_entity):
+        channel_id=channel_entity.id
+        query=(
+            f"SELECT from_id FROM ID_{channel_id}"
+            "WHERE from_id IS NOT NULL"
+        )
+        df=pd.read_sql(query,con=self.pd_conn)
         with TelegramClient("tmp", API().app_id, API().api_hash) as client:
-            client: T=client
-            i=0
-            keep_on_going=True
-            while keep_on_going:
-                tmp:ChannelMessages = client(GetHistoryRequest(
-                    peer=channel_entity,
-                    limit=100,
-                    offset_date=None,
-                    offset_id=offset,
-                    max_id=0,
-                    min_id=0,
-                    add_offset=0,
-                    hash=0))
-                results=pd.DataFrame().from_records(tmp.to_dict()["messages"])
-                if i!=0:
-                    keep_on_going=results["date"].to_list()[-1]!=last_iteration_data
-                else:
-                    keep_on_going=True
-                last_iteration_data=results["date"].to_list()[-1]
-                if keep_on_going:
-                    print(i,results["date"].to_list()[-1])
-                    i+=1
-                    try:
-                        posts=pd.concat([posts,results])
-                    except:
-                        if not os.path.exists(f"errors/{channel_username}"):
-                            os.mkdir(f"errors/{channel_username}")
-                        results.to_csv(f"errors/{channel_username}/{i}.csv")
-                    offset+=limit
-                else:
-                    print(i,results["date"].to_list()[-1],posts["date"].to_list()[-1])
-            if os.path.exists(f"errors/{channel_username}"):
-                files=os.listdir(f"errors/{channel_username}/")
-                files=[pd.read_csv(f"errors/{channel_username}/"+i) for i in files]
-                posts=pd.concat([posts]+files,ignore_index=True)
-                shutil.rmtree(f"errors/{channel_username}",ignore_errors=True)
-            posts.to_csv(f"db/{channel_username}.csv")
-        return posts
+            channel_entities=[client.get_entity(i) for i in df["from_id"]]
+        return channel_entities
+
+    def get_existing_channels(self,conn) -> pd.DataFrame:
+        df=pd.read_sql("select * from dumped_channels",con=conn)
+        return df
 
 if __name__=="__main__":
     pm="PoliticalMemes"
